102fdca450c44dca68ca8ecf0f99b8beb173cd5b
unzila <unzila31@gmail.com>
2021-06-23 12:52:36 +0500

    Update Parent.js

--- src/Parent.js
+++ src/Parent.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Child from './Child.js';
+import Child2 from './Child2.js';
 const Parent = () => {
     return(
+        <div>
         <Child />
+        <Child2 />
+        </div>
     )
 
 }
