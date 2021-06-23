dff76c6074e15d34ec60d61620c27bcb6d1aee56
unzila <unzila31@gmail.com>
2021-06-23 12:52:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import Parent from './Parent.js';
 import counterContext from './CounterContext';
+import { useState } from 'react';
 
 function App() {
+  // let [count, setCount] = useState(25);
+  let countState = useState(1);
   return (
-    <counterContext.Provider value={20}>
+    <counterContext.Provider value={countState}>
     <div className="App">
       <Parent Name="Unzila"/>
 
